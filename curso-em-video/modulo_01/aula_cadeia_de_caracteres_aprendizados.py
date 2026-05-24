22cb20333a46a443c9427dc6bdbb14766a69da11
JhannyARP <aparecidajhanny@gmail.com>
2026-05-23 22:56:02 -0300

    Na aula 'Manipulando texto 09'

---
+++ curso-em-video/modulo_01/aula_cadeia_de_caracteres_aprendizados.py
@@ -0,0 +1,55 @@
+frase = 'Curso em Vídeo Python'
+print(frase[3])
+
+frase = 'Curso em Vídeo Python'
+print(frase[3:13])
+
+frase = 'Curso em Vídeo Python'
+print(frase[:13])
+
+frase = 'Curso em Vídeo Python'
+print(frase[3:])
+
+frase = 'Curso em Vídeo Python'
+print(frase[1:15:2])
+
+frase = 'Curso em Vídeo Python'
+print(frase[::2])
+
+print("""Gato (nome científico: Felis catus) ou gato doméstico é um 
+mamífero carnívoro da família dos felídeos, muito popular 
+como animal de estimação. Ocupando o topo da cadeia alimentar,
+é predador natural de diversos animais, como roedores, pássaros,
+lagartixas e alguns insetos. Segundo pesquisas realizadas por
+instituições norte-americanas, os gatos consistem no segundo 
+animal de estimação mais popular do mundo, estando numericamente
+atrás apenas dos peixes de aquário. Consta em trigésimo nono
+na lista das 100 das espécies exóticas invasoras mais daninhas 
+do mundo da União Internacional para a Conservação da Natureza
+(UICN).""")
+
+frase = 'Curso em Vídeo Python'
+print(frase.upper().count('O'))
+
+frase = 'Curso em Vídeo Python'
+print(len(frase))
+
+frase = '  Curso em Vídeo Python      '
+print(len(frase))
+
+frase = '     Curso em Vídeo Python      '
+print(len(frase.strip()))
+
+frase = 'Curso em Vídeo Python'
+print(frase.replace('Python', 'Android'))
+
+## Parei em 35:44
+
+
+
+
+
+
+
+
+
